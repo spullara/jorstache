657da736882dfc76083f29d5c8b9efb41c6d3eac
spullara <sam@sampullara.com>
2010-08-02 09:22:28 -0700

    move initialization to external

--- src/main/java/com/github/jorstache/CodebehindHandler.java
+++ src/main/java/com/github/jorstache/CodebehindHandler.java
@@ -28,7 +28,6 @@ public class CodebehindHandler extends MustacheHandler {
 
   public CodebehindHandler(File root, String templatePath, String codePath, String classname) throws MustacheException {
     super(root, templatePath, codePath, classname);
-    getCode();
   }
 
   public CodebehindHandler(File root, String templatePath, Class<?> clazz) throws MustacheException {
